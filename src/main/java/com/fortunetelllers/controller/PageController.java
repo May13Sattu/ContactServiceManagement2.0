307a2660ff3c9724f98303c4e898f90df652a2ee
Sattu <Satyamjawalkar@gmail.com>
2025-03-29 00:11:27 +0530

    First Commit

---
+++ src/main/java/com/fortunetelllers/controller/PageController.java
@@ -0,0 +1,20 @@
+package com.fortunetelllers.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/tempcontroller")
+public class PageController {
+
+    @GetMapping("/index")
+    public String getMethod(Model model){
+        System.out.println("Hello project building started");
+        //Sending data to view
+        model.addAttribute("name","Cantanct Management Services");
+        model.addAttribute("Youtube", "Learning code with Sattu");
+        return "index";
+    }
+}
