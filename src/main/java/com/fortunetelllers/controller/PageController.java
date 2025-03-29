e3c29eddd2476048c9cb3b5256c8b956aa277a15
Sattu <Satyamjawalkar@gmail.com>
2025-03-30 01:41:36 +0530

    Thyleaf Fragments

--- src/main/java/com/fortunetelllers/controller/PageController.java
+++ src/main/java/com/fortunetelllers/controller/PageController.java
@@ -5,16 +5,37 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
-@RequestMapping("/tempcontroller")
+@RequestMapping("/usersservice")
 public class PageController {
 
-    @GetMapping("/index")
-    public String getMethod(Model model){
+
+    // home page
+    @GetMapping("/home")
+    public String getHomePage(Model model){
         System.out.println("Hello project building started");
         //Sending data to view
         model.addAttribute("name","Cantanct Management Services");
         model.addAttribute("Youtube", "Learning code with Sattu dada");
         return "index";
     }
+
+    // about page
+
+    @GetMapping("/about")
+    public String getAboutPage(Model model){
+        model.addAttribute("isActive",false);
+        System.out.println("About page is called");
+        return "about"; // about page
+    }
+
+    // service page
+    @GetMapping("/services")
+    public String getSevicePage(Model model){ 
+        System.out.println("Service page is called");
+        //Sending data to view
+        return "service"; // service page
+    }
+    
 }
